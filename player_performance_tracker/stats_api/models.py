2e6c7918adb5b38693d6c6820ebd932b188f78b1
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-11-14 20:18:05 +0100

    Add player class

--- player_performance_tracker/stats_api/models.py
+++ player_performance_tracker/stats_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+# Create your models here.
+class Player(models.Model):
+    POSITIONS = [
+        ("PG ", "Point guard"),
+        ("SG", "Shooting guard"),
+        ("SF", "Smallforward"),
+        ("PF", "Power forward"),
+        ("C", "Center")
+    ]
+
+    playerName = models.CharField(
+        max_length=20, null=False, verbose_name="player's  name")
+    position = models.CharField(max_length=2, choices=POSITIONS)
+
+

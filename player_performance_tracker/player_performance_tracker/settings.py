bc7e614dfcb978847d0d560a09b4bca1e6e0ee11
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-11-14 19:06:51 +0100

    Make database in memory

--- player_performance_tracker/player_performance_tracker/settings.py
+++ player_performance_tracker/player_performance_tracker/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'player_performance_tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',
     }
 }
 

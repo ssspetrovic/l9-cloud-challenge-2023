7c826642258533866996f29176ab8452f097be69
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-11-14 22:17:02 +0100

    Finalize model layout, add simple demo in views

--- player_performance_tracker/stats_api/models.py
+++ player_performance_tracker/stats_api/models.py
@@ -23,9 +23,11 @@ class Player(models.Model):
 
 
 class PlayerStats(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-
+    player = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name="statistics")
+    
     # Regular statistics
+    games_played = models.IntegerField(default=0, verbose_name="games played")
     ftm = models.FloatField(default=0.0, verbose_name="free throw made")
     fta = models.FloatField(default=0.0, verbose_name="free throw attempted")
     two_pm = models.FloatField(default=0.0, verbose_name="two points made")
@@ -43,15 +45,15 @@ class PlayerStats(models.Model):
     # Derived statistics
     @property
     def ftp(self):
-        return (self.ftm / self.fta) * 100 if self.fta else 0
+        return round((self.ftm / self.fta) * 100 if self.fta else 0, 1)
 
     @property
     def two_pp(self):
-        return (self.two_pm / self.two_pa) * 100 if self.two_pa else 0
+        return round((self.two_pm / self.two_pa) * 100 if self.two_pa else 0, 1)
 
     @property
     def three_pp(self):
-        return (self.three_pm / self.three_pa) * 100 if self.three_pa else 0
+        return round((self.three_pm / self.three_pa) * 100 if self.three_pa else 0, 1)
 
     @property
     def pts(self):
@@ -59,22 +61,57 @@ class PlayerStats(models.Model):
 
     @property
     def val(self):
-        return (self.ftm + 2 * self.two_pm + 3 * self.three_pm + self.reb + self.blk + self.ast + self.stl) - (self.fta - self.ftm + self.two_pa - self.two_pm + self.three_pa - self.three_pm + self.to)
+        return round((self.ftm + 2 * self.two_pm + 3 * self.three_pm + self.reb + self.blk + self.ast + self.stl) - (self.fta - self.ftm + self.two_pa - self.two_pm + self.three_pa - self.three_pm + self.to), 1)
 
     @property
     def efgp(self):
-        return ((self.two_pm + self.three_pm + 0.5 * self.three_pm) / (self.two_pa + self.three_pa)) * 100 if (self.two_pa + self.three_pa) else 0
+        return round(((self.two_pm + self.three_pm + 0.5 * self.three_pm) / (self.two_pa + self.three_pa)) * 100 if (self.two_pa + self.three_pa) else 0, 1)
 
     @property
     def tsp(self):
-        return (self.pts / (2 * (self.two_pa + self.three_pa + 0.475 * self.fta))) * 100 if (2 * (self.two_pa + self.three_pa + 0.475 * self.fta)) else 0
+        return round((self.pts / (2 * (self.two_pa + self.three_pa + 0.475 * self.fta))) * 100 if (2 * (self.two_pa + self.three_pa + 0.475 * self.fta)) else 0, 1)
 
     @property
     def hastp(self):
-        return (self.ast / (self.two_pa + self.three_pa + 0.475 * self.fta + self.ast + self.to)) * 100 if (self.two_pa + self.three_pa + 0.475 * self.fta + self.ast + self.to) else 0
+        return round((self.ast / (self.two_pa + self.three_pa + 0.475 * self.fta + self.ast + self.to)) * 100 if (self.two_pa + self.three_pa + 0.475 * self.fta + self.ast + self.to) else 0, 1)
 
     class Meta:
         verbose_name_plural = "player stats"
 
     def __str__(self):
         return f"{self.player.player_name}'s statistics"
+
+    def to_dict(self):
+        return {
+            "playerName": self.player.player_name,
+            "gamesPlayed": self.games_played,
+            "traditional": {
+                "freeThrows": {
+                    "attempts": self.fta,
+                    "made": self.ftm,
+                    "shootingPercentage": self.ftp,
+                },
+                "twoPoints": {
+                    "attempts": self.two_pa,
+                    "made": self.two_pm,
+                    "shootingPercentage": self.two_pp,
+                },
+                "threePoints": {
+                    "attempts": self.three_pa,
+                    "made": self.three_pm,
+                    "shootingPercentage": self.three_pp,
+                },
+                "points": self.pts,
+                "rebounds": self.reb,
+                "blocks": self.blk,
+                "assists": self.ast,
+                "steals": self.stl,
+                "turnovers": self.to,
+            },
+            "advanced": {
+                "valorization": self.val,
+                "effectiveFieldGoalPercentage": self.efgp,
+                "trueShootingPercentage": self.tsp,
+                "hollingerAssistRatio": self.hastp,
+            },
+        }
--- player_performance_tracker/stats_api/views.py
+++ player_performance_tracker/stats_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import Player, PlayerStats
+
 
 # Create your views here.
+def index(response):
+    player, created = Player.objects.get_or_create(
+        player_name="Leeroy Jankins")
+    if created:
+        return HttpResponse("No luck!")
+
+    stats = player.statistics.first()
+    return JsonResponse(stats.to_dict())

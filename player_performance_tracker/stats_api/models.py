6f4a33d959c8e347845f3faa79b404caad832011
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-11-14 20:18:26 +0100

    Add stats class

--- player_performance_tracker/stats_api/models.py
+++ player_performance_tracker/stats_api/models.py
@@ -19,3 +19,32 @@ class Player(models.Model):
     position = models.CharField(max_length=2, choices=POSITIONS)
 
 
+class PlayerStats(models.Model):
+    # Regular statistics
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    games_played = models.IntegerField(default=0)
+
+    ft_attempts = models.FloatField(default=0.0)
+    ft_made = models.FloatField(default=0.0)
+    ft_percentage = models.FloatField(default=0.0)
+
+    two_pt_attempts = models.FloatField(default=0.0)
+    two_pt_made = models.FloatField(default=0.0)
+    two_pt_percentage = models.FloatField(default=0.0)
+
+    three_pt_attempts = models.FloatField(default=0.0)
+    three_pt_made = models.FloatField(default=0.0)
+    three_pt_percentage = models.FloatField(default=0.0)
+
+    points = models.FloatField(default=0.0)
+    rebounds = models.FloatField(default=0.0)
+    blocks = models.FloatField(default=0.0)
+    assists = models.FloatField(default=0.0)
+    steals = models.FloatField(default=0.0)
+    turnovers = models.FloatField(default=0.0)
+
+    # Advanced statistics
+    valorization = models.FloatField(default=0.0)
+    effective_field_goal_percentage = models.FloatField(default=0.0)
+    true_shooting_percentage = models.FloatField(default=0.0)
+    hollinger_assist_ratio = models.FloatField(default=0.0)

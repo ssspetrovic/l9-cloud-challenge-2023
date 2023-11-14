3790d3f790c16ca810d381e9f5a3b1daaeb54dc2
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-11-14 20:34:41 +0100

    Make base permanent temporarily

--- player_performance_tracker/player_performance_tracker/settings.py
+++ player_performance_tracker/player_performance_tracker/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'player_performance_tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        # 'NAME': ':memory:',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
